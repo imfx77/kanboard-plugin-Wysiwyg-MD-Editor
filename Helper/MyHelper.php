f61847975c3635d25c1a4ba6fd14e6e80c8a9e13
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-16 06:31:09 +0100

    Add Embed SVG Fly Method to Helper
     - for inline SVG

--- Helper/MyHelper.php
+++ Helper/MyHelper.php
@@ -5,13 +5,37 @@ namespace Kanboard\Plugin\PluginNameExampleStudlyCaps\Helper;
 use Kanboard\Core\Base;
 
 /**
- * My Helper
+ * Plugin PluginNameExampleStudlyCaps
+ * Class MyHelper
  *
- * @package  Helper
+ * @package  Plugin\PluginNameExampleStudlyCaps\Helper
  * @author   
  */
 class MyHelper extends Base
 {
+    /**
+     * Get SVG Icons for Inline Markup and CSS Styling (Embed SVG Fly Method)
+     *
+     * Include filename as class inside SVG file
+     * Set 'color' as 'currentColor' to use CSS or define a color
+     * Style using normal CSS
+     * @uses    $this->helper->supportHelper->embedSVGIcon('icon-filename-without-extension')
+     * @var     $svgFilename
+     * @return  svg
+     * @author  Erik Moberg https://www.erikmoberg.net/article/embed-svg-icons-in-html-with-php
+     * @author  aljawaid
+     */
+    public function embedSVGIcon($icon_filename)
+    {
+        if ($this->router->getPlugin()) {
+            // For plugin templates
+            return file_get_contents('plugins/' . $this->helper->app->getPluginName() . '/Assets/icons/' . $icon_filename . '.svg');
+        } else {
+            // For core templates
+            return file_get_contents('plugins/PluginNameExampleStudlyCaps/Assets/icons/' . $icon_filename . '.svg');
+        }
+    }
+
     /**
      * Summary
      *
