5ab26977737c7b0cea3e92ca7fb010fbdc91962c
Im[F(x)] <imfx77@hotmail.com>
2024-02-11 04:55:42 +0200

    * adding first translations for en_US

--- Locale/en_US/translations.php
+++ Locale/en_US/translations.php
@@ -4,27 +4,17 @@ return array(
     //
     // GENERAL
     //
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
+    'WysiwygMDEditor_PLUGIN_DESCRIPTION' => 'Integrates external MD editors into Kanboard in order to conveniently edit/preview
+                                            (and eventually render) the rich text enabled fields in the interface.
+                                            Every editor may allow for different customizations of functionality, MD features, and UI themes.',
+    'WysiwygMDEditor_NAME' => 'Wysiwyg MD Editor',
+    'WysiwygMDEditor_TITLE_SETTINGS' => 'Wysiwyg MD Editor settings',
+    'WysiwygMDEditor_TITLE_PREVIEW' => 'Wysiwyg MD Editor preview',
+    'WysiwygMDEditor_LINK_SETTINGS_PREVIEW' => 'Preview with Saved Settings',
+    'WysiwygMDEditor_LINK_SETTINGS_CHANGE' => 'Change Settings',
+    'WysiwygMDEditor_CONTAINER_PREVIEW' => 'Preview Container',
     //
     // CORE OVERRIDES OR 3RD PARTY PLUGIN TRANSLATIONS AFFECTING THIS PLUGIN
     //
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
-    '' => '',
+    //'' => '',
 );
