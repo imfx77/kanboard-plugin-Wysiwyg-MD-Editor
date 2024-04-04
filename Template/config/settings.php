c4ed42774451e668cc640cbdd13b175ad532838a
Im[F(x)] <imfx77@hotmail.com>
2024-04-04 23:50:31 +0300

    * changed description secions in settings to markdown * fixing styles

--- Template/config/settings.php
+++ Template/config/settings.php
@@ -1,7 +1,7 @@
 <style>
 .fieldset-WysiwygMDEditor {border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px;}
-.icon-WysiwygMDEditor {border: none; padding-right: 10px; padding-left: 30px;}
-.descr-WysiwygMDEditor {border: none; width: 100%;}
+.icon-WysiwygMDEditor {border: none; padding-right: 10px; padding-left: 10px;}
+.descr-WysiwygMDEditor {border: none; width: 100%; padding-left: 10px;}
 </style>
 
 <div class="page-header">
@@ -15,7 +15,7 @@
 
     <fieldset class="fieldset-WysiwygMDEditor">
         <legend><?= t('WysiwygMDEditor_SETTINGS_EDITING_OPTIONS') ?></legend>
-        <div><?= t('WysiwygMDEditor_SETTINGS_EDITING_DESCR') ?></div>
+        <div class="descr-WysiwygMDEditor"><?= $this->helper->text->markdown(t('WysiwygMDEditor_SETTINGS_EDITING_DESCR')) ?></div>
         <br>
         <div class="icon-WysiwygMDEditor"><img src="<?= $this->helper->url->base() ?>plugins/WysiwygMDEditor/Screenshots/textarea-editing-toolbar-injected-buttons.png"></div>
 
@@ -28,14 +28,15 @@
                 </td>
                 <td class="descr-WysiwygMDEditor">
                     <a href="https://github.com/Ionaru/easy-markdown-editor" target="_blank" title="<?= t('Opens in a new window') ?>"> EasyMDE <i class="fa fa-external-link"></i></a>
-                    <br><?= t('WysiwygMDEditor_SETTINGS_EASYMDE_DESCR') ?>
+                    <br>
+                    <div><?= $this->helper->text->markdown(t('WysiwygMDEditor_SETTINGS_EASYMDE_DESCR')) ?></div>
 
                     <?php
                         if (!isset($values['WysiwygMDEditor_easymde_default_theme'])) {
                             $values['WysiwygMDEditor_easymde_default_theme'] = 'dimmed';
                         }
                     ?>
-                    <br><?= $this->form->label(t('WysiwygMDEditor_SETTINGS_EASYMDE_THEME_SELECT'), 'WysiwygMDEditor_easymde_default_theme') ?>
+                    <?= $this->form->label(t('WysiwygMDEditor_SETTINGS_EASYMDE_THEME_SELECT'), 'WysiwygMDEditor_easymde_default_theme') ?>
                     <?= $this->form->select('WysiwygMDEditor_easymde_default_theme', array(
                         'light'  => t('WysiwygMDEditor_SETTINGS_EASYMDE_THEME_LIGHT'),
                         'dimmed' => t('WysiwygMDEditor_SETTINGS_EASYMDE_THEME_DIMMED'),
@@ -54,7 +55,8 @@
                 </td>
                 <td class="descr-WysiwygMDEditor">
                     <a href="https://github.com/mafgwo/stackedit-plus" target="_blank" title="<?= t('Opens in a new window') ?>"> StackEdit+ <i class="fa fa-external-link"></i></a>
-                    <br><?= t('WysiwygMDEditor_SETTINGS_STACKEDIT_DESCR') ?>
+                    <br>
+                    <div><?= $this->helper->text->markdown(t('WysiwygMDEditor_SETTINGS_STACKEDIT_DESCR')) ?></div>
                 </td>
             </tr></table>
         </div>
