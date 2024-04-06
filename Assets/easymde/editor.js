264a911a9f5ac665bbbb9d1e69b3afe4d47d3e19
Im[F(x)] <imfx77@hotmail.com>
2024-04-07 01:17:24 +0300

    * hiding a globally visible constant within the scope of a function

--- Assets/easymde/editor.js
+++ Assets/easymde/editor.js
@@ -2,18 +2,18 @@
  * @author  Im[F(x)]
  */
 
-const easymdeHtmlContainer = `
-<div class="easymde-iframe-container">
-  <button class="easymde-close-button" title="Close">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%">
-      <path fill="#777" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-    </svg>
-  </button>
-  <iframe class="easymde-iframe"></iframe>
-</div>
-`;
-
 function injectButtonWrapperEasyMDE() {
+
+    const easymdeHtmlContainer = `
+    <div class="easymde-iframe-container">
+      <button class="easymde-close-button" title="Close">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%">
+          <path fill="#777" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+        </svg>
+      </button>
+      <iframe class="easymde-iframe"></iframe>
+    </div>
+    `;
 
     $('.text-editor-write-mode').each(function() {
 
