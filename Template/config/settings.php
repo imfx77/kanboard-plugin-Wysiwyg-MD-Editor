7e2671b6c7a090691067de47f265970f9aacb971
Im[F(x)] <imfx77@hotmail.com>
2024-09-23 20:24:14 +0300

    * added github buttons to settings page

--- Template/config/settings.php
+++ Template/config/settings.php
@@ -268,3 +268,26 @@ details summary {cursor: pointer;}
     <!-------------------------------------------------------------------------------------------------------->
 
 </form>
+
+<!-------------------------------------------------------------------------------------------------------->
+<!-- GITHUB BUTTONS -->
+<script src="/plugins/WysiwygMDEditor/vendor/buttons/github-buttons/buttons.min.js"></script>
+<br>
+<div>
+
+<!-- Issue -->
+<a class="github-button" href="https://github.com/imfx77/kanboard-plugin-Wysiwyg-MD-Editor/issues" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-issue-opened" data-size="large" aria-label="Issue imfx77/kanboard-plugin-Wysiwyg-MD-Editor on GitHub">Issue</a>
+
+<!-- Discuss -->
+<a class="github-button" href="https://github.com/imfx77/kanboard-plugin-Wysiwyg-MD-Editor/discussions" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-comment-discussion" data-size="large" aria-label="Discuss imfx77/kanboard-plugin-Wysiwyg-MD-Editor on GitHub">Discuss</a>
+
+<!-- Star -->
+<a class="github-button" href="https://github.com/imfx77/kanboard-plugin-Wysiwyg-MD-Editor" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" aria-label="Star imfx77/kanboard-plugin-Wysiwyg-MD-Editor on GitHub">Star</a>
+
+<!-- Follow -->
+<a class="github-button" href="https://github.com/imfx77" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Follow @imfx77 on GitHub">Follow @imfx77</a>
+
+<!-- Sponsor -->
+<a class="github-button" href="https://github.com/sponsors/imfx77" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-heart" data-size="large" aria-label="Sponsor @imfx77 on GitHub">Sponsor</a>
+
+</div>
